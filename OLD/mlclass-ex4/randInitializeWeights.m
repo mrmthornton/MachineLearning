bd91309b5e746c8cc6b3587538367b39dbaeb0aa
mrmthornton <mrmthornton@gmail.com>
2014-10-28 15:03:28 -0500

    Class 7

---
+++ OLD/mlclass-ex4/randInitializeWeights.m
@@ -0,0 +1,27 @@
+function W = randInitializeWeights(LayerIn, LayerOut)
+%RANDINITIALIZEWEIGHTS Randomly initialize the weights of a layer with L_in
+%incoming connections and L_out outgoing connections
+%   W = RANDINITIALIZEWEIGHTS(L_in, L_out) randomly initializes the weights 
+%   of a layer with L_in incoming connections and L_out outgoing 
+%   connections. 
+%
+%   Note that W should be set to a matrix of size(L_out, 1 + L_in) as
+%   the column row of W handles the "bias" terms
+%
+
+% You need to return the following variables correctly 
+W = zeros(LayerOut, 1 + LayerIn);
+
+% Randomly initialize the weights to small values
+% ====================== YOUR CODE HERE ======================
+% Instructions: Initialize W randomly so that we break the symmetry while
+%               training the neural network.
+%
+% Note: The first row of W corresponds to the parameters for the bias units
+%
+epsilonInit = 0.12;
+W = rand(LayerOut, 1 + LayerIn) * 2 * epsilonInit - epsilonInit;
+
+% =========================================================================
+
+end
