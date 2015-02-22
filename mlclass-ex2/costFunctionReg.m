9a9a1f84da1c30ff36045a94bcd078a3d5acc076
mrmthornton <mrmthornton@gmail.com>
2015-02-22 06:50:30 -0600

    finished exercise 2

--- mlclass-ex2/costFunctionReg.m
+++ mlclass-ex2/costFunctionReg.m
@@ -6,7 +6,7 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 
 % Initialize some useful values
 m = length(y); % number of training examples
-
+n = length(theta); % number of feature plus 1 for theta_0.
 % You need to return the following variables correctly 
 J = 0;
 grad = zeros(size(theta));
@@ -17,10 +17,15 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+I0 = eye(n); I0(1,1) = 0; % this is to avoid regularizing theta_0
 
+Hypothesis = sigmoid(X * theta);
 
+J = -1/m * ( y' * log(Hypothesis) \           % y = 1
+	+ (1-y') * log(1-Hypothesis)) \        % y = 0
+	+ lambda/(2*m) * theta' * I0 * theta;  % penalty for large theta
 
-
+grad = 1/m * X' * (Hypothesis - y) + lambda/m * I0 * theta;
 
 % =============================================================
 
